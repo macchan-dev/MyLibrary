aa40e6a13f7e0f7657390ba497b3228bae1994f5
macchan.dev <macchan.dev@gmail.com>
2014-10-13 16:10:14 +0900

    add project.

---
+++ app/src/androidTest/java/com/macchan_dev/mylibrary/ApplicationTest.java
@@ -0,0 +1,19 @@
+package com.macchan_dev.mylibrary;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+
+    public void testGetId() throws Exception {
+        MyLibrary my = new MyLibrary();
+        int id = my.getId();
+        assertEquals(id, 10);
+    }
+}
---
+++ app/src/main/java/com/macchan_dev/mylibrary/MyLibrary.java
@@ -0,0 +1,20 @@
+package com.macchan_dev.mylibrary;
+
+/**
+ * Created by fox on 2014/10/13.
+ */
+public class MyLibrary {
+    private int id = 0;
+
+    public MyLibrary() {
+        id = 1;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
